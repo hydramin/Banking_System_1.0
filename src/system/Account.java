32c21d4ba6bdbfd3d375f6e695ebf5c97a525d9f
hydramin <hydramin@my.yorku.ca>
2017-10-07 16:10:50 -0400

    javadocs added to Customer

--- src/system/Account.java
+++ src/system/Account.java
@@ -2,4 +2,10 @@ package system;
 
 public class Account {
 
+    private double balance; // the current amount in the account
+    private int accountNumber; // identification for an account
+    private  double withdrawAmount;
+    private double depositAmount;
+
+
 }
--- src/system/Chequeing.java
+++ src/system/Chequeing.java
@@ -1,5 +1,7 @@
 package system;
 
 public class Chequeing extends Account {
-
+    private final byte overdraftOption1 = 1; // 1 , 2 , 3 for each options
+    private final byte overdraftOption2 = 2; // 1 , 2 , 3 for each options
+    private final byte overdraftOption3 = 3; // 1 , 2 , 3 for each options
 }
