57e88e5d7cd9cb146a29df89e3bc665f2d03e59c
git-Sohrab <sohrab1997@outlook.com>
2017-10-08 23:45:17 -0400

    made some changes -- more to come :)

--- src/system/Account.java
+++ src/system/Account.java
@@ -1,6 +1,6 @@
 package system;
 
-public class Account implements accountOperations{
+public class Account implements accountOperations {
 	private double balance; // the current amount in the account
     private int accountNumber; // identification for an account
     private  double withdrawAmount;
--- src/system/Customer.java
+++ src/system/Customer.java
@@ -3,17 +3,19 @@ package system;
 
 import java.util.HashMap;
 
-public class Customer {
+public class Customer
+{
 
-	private int securityNumber;
+	private final int securityNumber;
 	private Account cheque;
 	private Account credit;
 	private Account loan;
-	private static HashMap<Integer, Customer> customerList= new HashMap<>();
+	private static HashMap<Integer, Customer> customerList = new HashMap<>();
 
     /**
      * @Description: The constructor is only called by the addCustomer(..) method and it
      *              instantiates a new customer
+	 *
      * @param securityNumber: unique identifier for a single customer
      */
 	private Customer(int securityNumber) 
@@ -28,49 +30,71 @@ public class Customer {
 	//////////////////////////////////////////////////////////////////////////////////////////////
     // Getters
 
-    public static HashMap<Integer, Customer> getCustomerList() {
+    public static HashMap<Integer, Customer> getCustomerList()
+	{
         return customerList;
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
+	 *
      * @Description: This method is the only way to create a new customer. A new security number is passed
      *              as a parameter and if the customer is not in the system, it will be added as a new one.
      *              The new customer is then passed in to the list of customers map(customerList).
+	 *
+	 *
      * @param securityNumber: unique identifier for a single customer
+	 * @pre
+	 * @post
      * @return a new customer if the security number is not already registered in the system
+	 *
      */
 
 	public static Customer addCustomer(int securityNumber)
 	{
-		int key = securityNumber;
-		Customer c = Customer.customerList.get(key);
-		if (c == null)
+		/**
+		 * Why don't we try it like this?
+		 *
+		 */
+
+		if (!customerList.containsKey(securityNumber))
 		{
-			c = new Customer(securityNumber);
-			Customer.customerList.put(key, c);
+			return customerList.put(securityNumber, new Customer(securityNumber));
+
 		}
-		return c;
+		return customerList.get(securityNumber);
 	}
 
     /**
+	 *
      * @Description: The 3 types of accounts are instantiated by means of polymorphism and passed
      *              in as arguments, if the account is null it will be assigned to the parameter.
+	 *
      * @param account: it is either a Chequeing, Credit or Loan type
+	 * @pre
+	 * @post
      * @throws throws IllegalArgument Exception if parameter is null
+	 *
      */
+
 	public void addAccount(Account account)
 	{
-	    if(account != null)
+
+
+		// Just made some visual, preference changes lol, nothing major.
+
+		if(!account.equals(null))
 	    {
-            if (account instanceof Chequeing && this.cheque == null)
+            if (account instanceof Chequeing && this.cheque.equals(null))
                 this.cheque = account;
-            if (account instanceof Credit && this.credit == null)
+            if (account instanceof Credit && this.credit.equals(null))
                 this.credit = account;
-            if (account instanceof Loan && this.loan == null)
+            if (account instanceof Loan && this.loan.equals(null))
                 this.loan = account;
-        }else {
+        }
+        else
+		{
 	        throw new IllegalArgumentException();
         }
 	}
@@ -78,13 +102,16 @@ public class Customer {
 	//////////////////////////////////////////////////////////////
 
     /**
-     *
+	 *
+     * @Description: Return the string instance of this Customer
+	 *
      * @return the account types, null or not
+	 *
      */
+
     @Override
-    public String toString() {
-        return (cheque +" " + credit + " " + loan);
+    public String toString()
+	{
+        return String.format ("%s, %s, %s", cheque, credit, loan);		// we should try using String format
     }
-
-
 }
